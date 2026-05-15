42997941b44b88a017061bcefb4c61eea1bd036d
nmayock <noahmayock.it@gmail.com>
2026-05-15 15:23:28 -0400

    Clean up comments in realEstateAnalyzer.py
    Removed comments

--- realEstateAnalyzer.py
+++ realEstateAnalyzer.py
@@ -1,26 +1,4 @@
-# Noah Mayock
 # Assignment: Real Estate Analyzer 
-# Reflection:
-#  I enjoyed this assingment as a whole, I think the ability to work with CSVs is a great skill to have and has a plethora of applications. My dad is a real estate
-#  appraiser so I'd like to make this program more univerally usable with CSVs with variable metadata and structure, maybe using regex to parse the meta data and 
-#  assign the record[x] based on that.
-#
-#  Honestly this assignment wasn't too bad. It felt like a nice progression from the first two and I was able to use them as a frame work to start as well as   
-#  the python 1 version of this assignment. It was interesting to go back and look at that and see the difference in how I wrote the getMedian function then.
-#
-#  I used the csv module to import the data. It just made the most sense and of course we had your example. The only thing I changed was using next rather than
-#  [1:] to skip the header/metadata and adding error handling for FileNotFound.
-#
-#  I used 4 dictionaries. I could have used 3 and done print(f"...") to out put the general summary data but I had already written a dictionary based summary function 
-#  so I figured why not just put that data in a dict as well.
-#
-#  1. I learned to different ways to update dictionary values, the one seen below and dict.setdefaults(). I'm not sure which is better, but I went with the method you
-#     showed in the help session because I only learned about the dict.setdefaults from Tyler/substack and didnt want to use something from an outside source or copy
-#     Tylers idea.
-#  2. This is somewhat outside the scope of the actual assignment but Tyler was under the weather the past week so I tried to lighten his load and help people with
-#     questions/issues. It was illuminating. Huge respect to you, Tyler and past/present SI's because I did not expect it to be so difficult to help someone who is 
-#     explicitly asking for your help. I also found it was a fine line to walk trying to gently nudge them In the right direction without spoiling their learning      
-#     process or giving away the answer. Overall it was still very rewarding and I enjoyed seeing different peoples approaches.
 
 import csv
 from pathlib import Path 
