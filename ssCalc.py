1924adc5332d2a364bdcbf6a8854a14f64a486d3
nmayock <noahmayock.it@gmail.com>
2026-05-15 15:24:30 -0400

    Clean up comments in ssCalc.py
    Removed comments reflecting on SQL assignment and personal experiences.

--- ssCalc.py
+++ ssCalc.py
@@ -1,24 +1,4 @@
-# Noah Mayock
 # Assignment: SQL and Python
-#Reflection:
-# I have unfortunately allowed myself to get quite rusty in regards to SQL so this assignment was a nice way to re-introduce myself to the subject
-# 
-# Timing. I have burnt myself  out completely this semester, as I'm sure youll see by the time I finally get this in
-#
-# I knew this question sounded familiar, my appologies but I did copy and paste my answer from the last assignment. Also hopefully decorators werent a requirement for this one:
-# Decorators work by allowing a function to take another function as an arguement and return another new function. This allows coders to extend and enhance the 
-# capabilities of their functions without having to alter their original code. The property decorator allows you to turn class methods into managed attributes which
-# lets you access/"get" the methods value while the actual logic is handled internally/behind the scenes.
-#
-# DDL 'defines'm the structure of the database, creates the tables and defines there attributes, columns, rows, data type requirements etc. DML deals with the data in that
-# predefined structure. 
-#
-# The SELECT is deciding what tables to use and the join is linking them together based off shared collums
-#
-# 1. I technically learned this from being confused by the weekly course content examples but I did not know that the connection object to create a temporary cursor object rather#    than explicitly creating one. From what I've seen and I could probably definitely be wrong it is still best practice to create each object distinctly hense my code.
-# 2. Dont take 17 credits your in your final semeste for "fun". It will not be fun. That being said I have thuroughly enjoyed every course and class I've had with you and I
-#    consider myself incredibly lucky to have had you as a professor. Thank you, really and truly you have made an incredible impact on my life. 
-#
 
 import sqlite3
 import csv
