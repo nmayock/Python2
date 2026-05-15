1c89ead994066702b16e3c0ec899d72889fb4155
nmayock <noahmayock.it@gmail.com>
2026-05-15 15:18:24 -0400

    Clean up comments in NumerologyInheritanceUse.py
    Removed reflection comments from the NumerologyInheritanceUse.py file.

--- NumerologyInheritanceUse.py
+++ NumerologyInheritanceUse.py
@@ -1,21 +1,5 @@
 # Noah Mayock
 # Assignment: Numerology Inheritance
-# Reflection:
-# I really enjoyed how simple the assignment was. I don't mean conceptually simple but moreseo the execution/implementation felt very logical and clean in the way
-# that everything flowed/was inherited from the parent to the child to the use file. 
-#
-# I had a hard time with the placement of my meanings/descriptions. I originaly had them outside the class as a global constant would be but as you made clear, while it 
-# should be treated as a constant, it is NOT global and should not be accecible to other child classes.
-#
-# Decorators work by allowing a function to take another function as an arguement and return another new function. This allows coders to extend and enhance the 
-# capabilities of their functions without having to alter their original code. The property decorator allows you to turn class methods into managed attributes which
-# lets you access/"get" the methods value while the actual logic is handled internally/behind the scenes.
-#
-# 1. I learned about protected variables and their usage. After making my meanings dictionary a protected variable I had tried just using _LP_DETAILS but thankfully 
-#    pythons error messages pointed me in the correct direction and I reallized I would need self. in order to access this since it was now a protected class variable
-#
-# 2. This was my first time using inheritance in python and I really enjoy it. It felt like a breakthrough as far as understanding the power of OOP and classes when
-#    creating modular programs.
 
 import NumerologyLifePathDetails as numIn
 import DateValidator as dv
